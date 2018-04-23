b60059138e464afffa93b918c187c53527492377
DANY211 <DANY@github.com>
2018-04-23 23:47:42 +0300

    add

--- src/Du/Du.java
+++ src/Du/Du.java
@@ -1,13 +1,15 @@
 package Du;
 
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Du {
-    privat final boolean sumSize;
-    privat final boolean format;
-    privat final boolean unit;
+    private final boolean sumSize;
+    private final boolean format;
+    private final boolean unit;
 
 
     public Du(boolean sunSize, boolean format, boolean unit) {
@@ -16,7 +18,7 @@ public class Du {
         this.unit = unit;
     }
 
-    privat
+    private
 
     long fileSize(File file) {
         if (!file.isDirectory()) return file.length();
@@ -30,11 +32,54 @@ public class Du {
             return a;
         }
     }
-    public list answer(String[] fileN){
-        String[] list = {"B","KB","MB","GB"};
-        List ansList = new ArrayList();
-        long a = 0;
+    public List answer(String[] fileN) {
+        String[] list = {"B", "KB", "MB", "GB"};
+        List answerList = new ArrayList();
+        long s = 0;
         long size;
+        int n = 1024;
+        int form;
+        if (unit) n = 1000;
+        for (int i = 0; i < fileN.length; i++) {
+            File file = new File(fileN[i]);
+            if (!file.exists()) {
+                answerList.clear();
+                answerList.add("1 - ошибка");
+                return answerList;
+            }
+            if (sumSize) s += fileSize(file);
+            else {
+                size = fileSize(file);
+                if (format) {
+                    form = 0;
+                    while (size / n > 0) {
+                        size /= n;
+                        form++;
+                    }
+                    answerList.add("Размер " + fileN[i] + " равен " + size + " " + list[form]);
+                } else {
+                    size /= n;
+                    answerList.add("Размер " + fileN[i] + " равен " + size);
+                }
+            }
+        }
+        if (sumSize) {
+            if (format) {
+                form = 0;
+                while (s / n > 0) {
+                    s /= n;
+                    form++;
+                }
+                answerList.add("Суммарный размер равен " + s + " " + list[form]);
+            } else {
+                s /= n;
+                answerList.add("Суммарный размер равен " + s);
+            }
+        }
+        return answerList;
+    }
 
+    public void outWriter(List ansList) {
+        for (int i = 0; i < ansList.size(); i++) System.out.println(ansList.get(i));
     }
 }
--- src/Launch/Launch.java
+++ src/Launch/Launch.java
@@ -1,5 +1,47 @@
 package Launch;
 
-public class Launch{
+import Du.Du ;
+import java.util.ArrayList;
+import java.util.List;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+public class Launch {
+    @Option(name = "-h", usage = "Normal format")
+    private boolean format = false;
+
+    @Option(name = "-c", usage = "Size")
+    private boolean sumSize = false;
+
+    @Option(name = "-si", usage = "Another unit")
+    private boolean unit = false;
+
+    @Argument(metaVar = "fileN", usage = "Input file names")
+    private String[] fileN;
+
+    public static List list = new ArrayList();
+
+    public static void main(String[] args) {
+        new Launch().launch(args);
+    }
+
+    private void launch(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("_____");
+            parser.printUsage(System.err);
+            return;
+        }
+
+        Du du = new Du(format, sumSize, unit);
+        du.outWriter(du.answer(fileN));
+        System.out.println("End");
+        list = du.answer(fileN);
+    }
 
 }
